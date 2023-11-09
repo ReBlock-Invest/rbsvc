19c48544e52939363c81a7d6345bc7afc01926cc
Rifqi Alfian <merembablas@gmail.com>
2023-11-09 14:43:40 +0700

    fix: nonce and verify signature

--- src/login.rs
+++ src/login.rs
@@ -1,6 +1,10 @@
 use actix_web::{http::header::ContentType, web, HttpResponse};
+use ethers::prelude::*;
+use ethers::utils::keccak256;
+use redis::Commands;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::str::FromStr;
 
 #[derive(Deserialize, Debug)]
 pub struct NonceReq {
@@ -25,27 +29,23 @@ pub struct LoginResp {
 }
 
 fn connect() -> redis::Connection {
-    //format - host:port
-    let redis_host_name =
-        env::var("REDIS_HOSTNAME").expect("missing environment variable REDIS_HOSTNAME");
+    let redis_url = env::var("REDIS_URL").expect("missing environment variable REDIS_URL");
 
-    let redis_password = env::var("REDIS_PASSWORD").unwrap_or_default(); //if Redis server needs secure connection
-    let uri_scheme = match env::var("IS_TLS") {
-        Ok(_) => "rediss",
-        Err(_) => "redis",
-    };
-    let redis_conn_url = format!("{}://:{}@{}", uri_scheme, redis_password, redis_host_name);
-    println!("{}", redis_conn_url);
-    redis::Client::open(redis_conn_url)
+    redis::Client::open(redis_url)
         .expect("Invalid connection URL")
         .get_connection()
         .expect("failed to connect to Redis")
 }
 
-pub async fn nonce(form: web::Json<NonceReq>) -> HttpResponse {
+pub async fn nonce(data: web::Json<NonceReq>) -> HttpResponse {
+    let mut conn = connect();
+
+    let key = format!("rb:nonce:{}", data.address);
+    let n_data: u64 = conn.get(&key).unwrap();
+
     let nonce = NonceResp {
-        nonce: 1,
-        address: form.address.to_owned(),
+        nonce: n_data,
+        address: data.address.to_owned(),
     };
 
     HttpResponse::Ok()
@@ -54,14 +54,57 @@ pub async fn nonce(form: web::Json<NonceReq>) -> HttpResponse {
 }
 
 pub async fn wallet_login(data: web::Json<LoginReq>) -> HttpResponse {
-    println!("{:?}", data.address);
-    println!("{:?}", data.signature);
+    let mut conn = connect();
+    let key = format!("rb:nonce:{}", data.address);
+    let n_data: u64 = conn.get(&key).unwrap();
+
+    let signature = Signature::from_str(&data.signature).expect("Invalid signature format");
+    let message = eth_message(n_data.to_string());
+    let address = Address::from_str(&data.address).expect("Invalid Ethereum address format");
+
+    let is_valid = signature
+        .verify(n_data.to_string().as_str(), address)
+        .is_ok();
+
+    if !is_valid {
+        return HttpResponse::BadRequest()
+            .content_type(ContentType::json())
+            .body("{}");
+    }
+
+    if let Ok(recovered_public_key) = signature.recover(message) {
+        let rec_address = Address::from(recovered_public_key);
+
+        if rec_address != address {
+            return HttpResponse::BadRequest()
+                .content_type(ContentType::json())
+                .body("{}");
+        }
+    } else {
+        return HttpResponse::BadRequest()
+            .content_type(ContentType::json())
+            .body("{}");
+    }
 
     let resp = LoginResp {
         access_token: String::from("AaaJhHHJ"),
     };
 
+    let _: i64 = conn.incr(&key, 1).unwrap();
+
     HttpResponse::Ok()
         .content_type(ContentType::json())
         .json(resp)
+}
+
+fn eth_message(message: String) -> [u8; 32] {
+    keccak256(
+        format!(
+            "{}{}{}",
+            "\x19Ethereum Signed Message:\n",
+            message.len(),
+            message
+        )
+        .as_bytes(),
+    )
 }
